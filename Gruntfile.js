a95a78cc0cc8876f1b99f7e5e7050a8e568e6165
Neo Ighodaro <neo@creativitykills.net>
2014-09-22 09:45:59 +0100

    Very minor improvement

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,6 +164,7 @@ module.exports = function(grunt)
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
+
 	// -----------------------------------------
 	// Register Grunt tasks
 	// -----------------------------------------
